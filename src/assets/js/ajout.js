33f4f52ed8d79bab2147169af8d151f7b8426c05
MhamedOuhanane <mhmdeouhnan60@gmail.com>
2024-11-28 15:46:15 +0100

    Ajouter l'affichage des badge aprés le clique sur bouton badge

--- src/assets/js/ajout.js
+++ src/assets/js/ajout.js
@@ -58,7 +58,7 @@ Ajounouv.addEventListener("click" , ()=>{
 conferjoue.onclick= () =>{
     mudalajout.classList.toggle("hidden");
     joueurs.players.forEach((Element) => {
-        cardsform.innerHTML +=`<div class="BadgeAjout ${"bg" + Element.position} relative w-[7rem] h-[10rem] hover:scale-95 flex flex-col  items-center py-4 text-white">
+        cardsform.innerHTML +=`<div class="BadgeAjout1 ${"bg" + Element.position} relative w-[7rem] h-[10rem] hover:scale-95 flex flex-col  items-center py-4 text-white">
                                 <div class="flex justify-start  w-full h-[75%] p-[12%]">
                                     <div class="max-w-7 flex flex-col items-center pt-3 z-1">
                                         <span  class="scorejou1 text-[110%]">${Element.rating}</span><span class="typescore1">${Element.position}</span>
@@ -126,20 +126,20 @@ conferjoue.onclick= () =>{
     });
 
     let inputname = document.querySelector("#mudalajout input");
-    let BadgeAjout = document.querySelectorAll(".BadgeAjout");
+    let BadgeAjout1 = document.querySelectorAll(".BadgeAjout1");
 
     //Filtrage des joueur par name
     inputname.oninput= ()=>{
         joueurs.players.forEach((Element , index) =>{
-            BadgeAjout[index].classList.remove("hidden");
+            BadgeAjout1[index].classList.remove("hidden");
             if (inputname.value == "") {
-                BadgeAjout[index].classList.remove("hidden");
+                BadgeAjout1[index].classList.remove("hidden");
             } else if (inputname.value == Element.name) {
                 if (inputname.value != Element.name) {
-                    BadgeAjout[index].classList.add("hidden");
+                    BadgeAjout1[index].classList.add("hidden");
                 }
             } else{
-                BadgeAjout[index].classList.add("hidden");
+                BadgeAjout1[index].classList.add("hidden");
 
             }
         });
@@ -147,17 +147,17 @@ conferjoue.onclick= () =>{
         //Filtrage des joueur par position
     position.oninput= ()=>{
         joueurs.players.forEach((Element , index) =>{
-            BadgeAjout[index].classList.remove("hidden");
+            BadgeAjout1[index].classList.remove("hidden");
             if (position.value == Element.position) {
                 if (position.value != Element.position) {
-                    BadgeAjout[index].classList.add("hidden");
+                    BadgeAjout1[index].classList.add("hidden");
                 }
             }
             
         });
     };
 
-    BadgeAjout.forEach((Element , index) =>{
+    BadgeAjout1.forEach((Element , index) =>{
         Element.onclick= () =>{
             if (effectif.value == "Effectif") {
                 effectif.classList.add("border-3px border-red")
@@ -186,7 +186,78 @@ conferjoue.onclick= () =>{
 
 
 //Ajouter a partire du botton de badge
+joueurs.players.forEach((Element) => {
+    cardsform.innerHTML +=`<div class="BadgeAjout2 ${"bg" + Element.position} relative w-[7rem] h-[10rem] hover:scale-95 flex flex-col  items-center py-4 text-white">
+                            <div class="flex justify-start  w-full h-[75%] p-[12%]">
+                                <div class="max-w-7 flex flex-col items-center pt-3 z-1">
+                                    <span  class="scorejou1 text-[110%]">${Element.rating}</span><span class="typescore1">${Element.position}</span>
+                                </div>
+                            </div>
+                            <img class="absolute  z-0 mb-1" src=${Element.photo}>
+                            <span class="z-10 mb-1 ">${Element.name}</span>
+                            <div class="flex justify-evenly gap-[0.2rem] md:gap-1 z-10 ${Element.position == "GK" ? "hidden" : ""}">
+                                <div class="flex flex-col items-center">
+                                    <span class="typescore1">PAC</span>
+                                    <span class="scorejou1">${Element.pace}</span>
+                                </div>
+                                <div class="flex flex-col items-center">
+                                    <span class="typescore1">SHO</span>
+                                    <span class="scorejou1">${Element.shooting}</span>
+                                </div>
+                                <div class="flex flex-col items-center">
+                                    <span class="typescore1">PAS</span>
+                                    <span class="scorejou1">${Element.passing}</span>
+                                </div>
+                                <div class="flex flex-col items-center">
+                                    <span class="typescore1">DRI</span>
+                                    <span class="scorejou1">${Element.dribbling}</span>
+                                </div>
+                                <div class="flex flex-col items-center">
+                                    <span class="typescore1">DEF</span>
+                                    <span class="scorejou1">${Element.defending}</span>
+                                </div>
+                                <div class="flex flex-col items-center">
+                                    <span class="typescore1">PHY</span>
+                                    <span class="scorejou1">${Element.physical}</span>
+                                </div>
+                            </div>
+                            <div class="flex justify-evenly gap-[0.2rem] md:gap-1 z-10 ${Element.position != "GK" ? "hidden" : ""}">
+                                <div class="flex flex-col items-center">
+                                    <span class="typescore1">DIV</span>
+                                    <span class="scorejou1">${Element.diving}</span>
+                                </div>
+                                <div class="flex flex-col items-center">
+                                    <span class="typescore1">HAN</span>
+                                    <span class="scorejou1">${Element.handling}</span>
+                                </div>
+                                <div class="flex flex-col items-center">
+                                    <span class="typescore1">KIC</span>
+                                    <span class="scorejou1">${Element.kicking}</span>
+                                </div>
+                                <div class="flex flex-col items-center">
+                                    <span class="typescore1">REF</span>
+                                    <span class="scorejou1">${Element.reflexes}</span>
+                                </div>
+                                <div class="flex flex-col items-center">
+                                    <span class="typescore1">SPE</span>
+                                    <span class="scorejou1">${Element.speed}</span>
+                                </div>
+                                <div class="flex flex-col items-center">
+                                    <span class="typescore1">POS</span>
+                                    <span class="scorejou1">${Element.positioning}</span>
+                                </div>
+                            </div
+                            <div class="flex justify-center gap-2  z-1">
+                                <img src=${Element.flag} class="w-[11%]" alt="flag">
+                                <img src=${Element.logo} class="w-[11%]" alt="logo">
+                            </div>
+                        </div>`;
+});
 
+let BadgeAjout2 = document.querySelectorAll(".BadgeAjout2");
 document.querySelectorAll(".badgeplus").forEach((Element , index) =>{
-    Element
+    Element.onclick= () => {
+        AjoutparBadge.classList.toggle("hidden");
+        
+    }
 });
