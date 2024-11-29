771c60aa6652af06a154e6989ce8c955df81ed30
MhamedOuhanane <mhmdeouhnan60@gmail.com>
2024-11-29 06:05:16 +0100

    Faire l'ajoute d'un nouveau joueur

--- src/assets/js/Regex.js
+++ src/assets/js/Regex.js
@@ -45,7 +45,7 @@ Ajounouv.addEventListener("click" , ()=>{
         //validation des input de type text (Regex)
     inputtext.forEach(Element =>{
         Element.oninput = () => {
-            if ( /^[a-zéèêàâùûîïöäüÿç ]{5,20}$/i.test(Element.value) ) {
+            if ( /^[a-zéèêàâùûîïöäüÿç\-\s]{5,20}$/i.test(Element.value) ) {
                 Element.classList.remove();
                 Element.className = "bg-green-200";
                 Validation = true;
@@ -80,18 +80,54 @@ Ajounouv.addEventListener("click" , ()=>{
             };
         };
     });
-    // Create.onclick= () =>{
-    //     //Validation des information des nouveau joueur ajouter
-    //     let inputtext = document.querySelectorAll('#mudalnouv input[type="Text"]');
-    //     let inputurl = document.querySelector('#mudalnouv input[type="url"]');
-    //     let inputnumber = document.querySelectorAll('#mudalnouv input[type="number"]');
+      //L'ajout des information au fichier js
+    console.log(document.querySelectorAll('#containerINPUT input'));
+    Create.onclick= () =>{
 
-    //     infjoueur.forEach(Element =>{
-    //         if (Element.value == "") {
-    //         }
-    //         else{
-                
-    //         }
-    //     });
-    // };
+    
+        if (Validation == true) {
+            let infor = document.querySelectorAll('#containerINPUT input');
+            
+            if (positionnouv.value != GK) {
+                nouvjoue = {
+                    name: infor[0].value,
+                    photo: "https://cdn3.futbin.com/content/fifa25/img/players/p50522519.png?fm=png&ixlib=java-2.1.0&verzion=1&w=485&s=f9ffb97b6af353daad5d7cb1ba1f6390",
+                    position: positionnouv.value,
+                    nationality: infor[2].value,
+                    flag: "https://cdn.sofifa.net/flags/br.png",
+                    club: infor[2].value,
+                    logo: "https://cdn.sofifa.net/meta/team/7011/120.png",
+                    rating: infor[4].value,
+                    pace: infor[5].value,
+                    shooting: infor[6].value,
+                    passing: infor[7].value,
+                    dribbling: infor[8].value,
+                    defending: infor[9].value,
+                    physical: infor[10].value
+    
+                }
+            } else {
+                    nouvjoue = {
+                        name: infor[0].value,
+                        photo: "https://cdn3.futbin.com/content/fifa25/img/players/p50522519.png?fm=png&ixlib=java-2.1.0&verzion=1&w=485&s=f9ffb97b6af353daad5d7cb1ba1f6390",
+                        position: "GK",
+                        nationality: infor[2].value,
+                        flag: "https://cdn.sofifa.net/flags/br.png",
+                        club: infor[3].value,
+                        logo: "https://cdn.sofifa.net/meta/team/7011/120.png",
+                        rating: infor[4].value,
+                        pace: infor[11].value,
+                        shooting: infor[12].value,
+                        passing: infor[13].value,
+                        dribbling: infor[14].value,
+                        defending: infor[15].value,
+                        physical: infor[16].value
+        
+                    }
+            }
+            
+            joueurs.players.push(nouvjoue);
+        }
+        
+    };
 });
