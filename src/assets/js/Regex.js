dee014ca72e45c6d591ba43c4d858a43f5e703b7
MhamedOuhanane <mhmdeouhnan60@gmail.com>
2024-11-29 14:04:45 +0100

    Mise à jour le regex pour l'input du url

--- src/assets/js/Regex.js
+++ src/assets/js/Regex.js
@@ -40,42 +40,45 @@ Ajounouv.addEventListener("click" , ()=>{
     //Validation des information des nouveau joueur ajouter
     let Validation = false;
     let inputtext = document.querySelectorAll('#mudalnouv input[type="Text"]');
+    console.log(inputtext);
+    
     let inputurl = document.querySelector('#mudalnouv input[type="url"]');
+    console.log(inputurl);
     let inputnumber = document.querySelectorAll('#mudalnouv input[type="number"]');
-        //validation des input de type text (Regex)
+    console.log(inputnumber);
+    //validation des input de type text (Regex)
     inputtext.forEach(Element =>{
-        Element.oninput = () => {
-            if ( /^[a-zéèêàâùûîïöäüÿç\-\s]{5,20}$/i.test(Element.value) ) {
-                Element.classList.remove();
-                Element.className = "bg-green-200";
+        Element.addEventListener("input" , () => {
+            if ( /^[a-zéèêàâùûîïöäüÿç\-\s]{5,20}$/i.test(Element.value) ) {              
+                Element.style.backgroundColor = "rgb(187 247 208)";
                 Validation = true;
             } else {
-                Element.classList.remove();
-                Element.className = "bg-red-300";
+                Element.style.backgroundColor = "rgb(254 202 202)";
                 Validation = false;
             };
-        };
+        });
     });
         //validation input de type url pour le photo
-    // inputurl.oninput = () => {
-    //     if ( /^[https:\/\/cdn3\.futbin\.com\/content\/fifa25\/img\/players\/p50522519\.png\?fm=png&ixlib=java-2\.1\.0&verzion]+[a-z0-9&=]/.test(inputurl.value) ) {
-    //         inputurl.style.color = "green";
-    //         Validation = true;
-    //     } else {
-    //         inputurl.style.color = "red";
-    //         Validation = false;
-    //     };
-    // }; 
-           //validation des input de type number (Regex)
+    inputurl.oninput = () => {
+        if ( /^https:\/\/cdn(?:3\.futbin\.com\/content\/fifa25\/img\/players\/p\d+\.png\?fm=png&ixlib=java-\d+\.\d+\.\d+&verzion=\d+&w=\d+&s=[a-f0-9]+|\.sofifa\.net\/players\/\d+\/\d+\/\d+_\d+\.png)$/
+        .test(inputurl.value) ) {
+            inputurl.style.backgroundColor = "rgb(187 247 208)";
+            Validation = true;
+        } else {
+            inputurl.style.backgroundColor = "rgb(254 202 202)";
+            Validation = false;
+        };
+    }; 
+        //    validation des input de type number (Regex)
     inputnumber.forEach(Element =>{
         Element.oninput = () => {
-            if ( /^[0-9][1-9]$/i.test(Element.value) ) {
+            if ( /^[0-9]{2}$/i.test(Element.value) ) {
                 Element.classList.remove();
-                Element.className = "bg-green-200";
+                Element.style.backgroundColor = "rgb(187 247 208)";
                 Validation = true;
             } else {
                 Element.classList.remove();
-                Element.className = "bg-red-300";
+                Element.style.backgroundColor = "rgb(254 202 202)";
                 Validation = false;
             };
         };
@@ -88,7 +91,7 @@ Ajounouv.addEventListener("click" , ()=>{
         if (Validation == true) {
             let infor = document.querySelectorAll('#containerINPUT input');
             
-            if (positionnouv.value != GK) {
+            if (positionnouv.value != "GK") {
                 nouvjoue = {
                     name: infor[0].value,
                     photo: "https://cdn3.futbin.com/content/fifa25/img/players/p50522519.png?fm=png&ixlib=java-2.1.0&verzion=1&w=485&s=f9ffb97b6af353daad5d7cb1ba1f6390",
@@ -116,17 +119,23 @@ Ajounouv.addEventListener("click" , ()=>{
                         club: infor[3].value,
                         logo: "https://cdn.sofifa.net/meta/team/7011/120.png",
                         rating: infor[4].value,
-                        pace: infor[11].value,
-                        shooting: infor[12].value,
-                        passing: infor[13].value,
-                        dribbling: infor[14].value,
-                        defending: infor[15].value,
-                        physical: infor[16].value
+                        diving: infor[11].value,
+                        handling: infor[12].value,
+                        kicking: infor[13].value,
+                        reflexes: infor[14].value,
+                        speed: infor[15].value,
+                        positioning: infor[16].value
         
                     }
             }
+            console.log(nouvjoue);
             
+            Erreur.classList.add("hidden");
             joueurs.players.push(nouvjoue);
+            console.log(joueurs);
+            
+        } else {
+            Erreur.classList.remove("hidden");
         }
         
     };
