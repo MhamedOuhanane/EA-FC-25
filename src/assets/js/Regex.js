5d23c1d4c3c245fd3f12e540fbdb1df17d7a9087
MhamedOuhanane <mhmdeouhnan60@gmail.com>
2024-11-29 04:18:52 +0100

    Ajouter la validation des information comme name, club et nationality des nouveau joueur

--- src/assets/js/Regex.js
+++ src/assets/js/Regex.js
@@ -36,18 +36,59 @@ Ajounouv.addEventListener("click" , ()=>{
     });
 
     let nouvjoue = {};
-    Create.onclick= () =>{
-        //Validation 
-        let inputtext = document.querySelectorAll('#mudalnouv input[type="Text"]');
-        let inputurl = document.querySelector('#mudalnouv input[type="url"]');
-        let inputnumber = document.querySelectorAll('#mudalnouv input[type="number"]');
 
-        infjoueur.forEach(Element =>{
-            if (Element.value == "") {
-            }
-            else{
+    //Validation des information des nouveau joueur ajouter
+    let Validation = false;
+    let inputtext = document.querySelectorAll('#mudalnouv input[type="Text"]');
+    let inputurl = document.querySelector('#mudalnouv input[type="url"]');
+    let inputnumber = document.querySelectorAll('#mudalnouv input[type="number"]');
+    //validation des input de type text
+    inputtext.forEach(Element =>{
+        Element.oninput = () => {
+            if ( /^[a-zéèêàâùûîïöäüç ]{5,20}$/i.test(Element) ) {
+                Element.style.color = "green";
+                Validation = true;
+            } else {
+                Element.style.color = "red";
+                Validation = false;
+            };
+        };
+    });
+    inputtext.forEach(Element =>{
+        Element.oninput = () => {
+            if ( /^[a-zéèêàâùûîïöäüç ]{5,20}$/i.test(Element) ) {
+                Element.style.color = "green";
+                Validation = true;
+            } else {
+                Element.style.color = "red";
+                Validation = false;
+            };
+        };
+    });
+    inputtext.forEach(Element =>{
+        Element.oninput = () => {
+            if ( /^[a-zéèêàâùûîïöäüç ]{5,20}$/i.test(Element) ) {
+                Element.style.color = "green";
+                Validation = true;
+            } else {
+                Element.style.color = "red";
+                Validation = false;
+            };
+        };
+    });
+
+    // Create.onclick= () =>{
+    //     //Validation des information des nouveau joueur ajouter
+    //     let inputtext = document.querySelectorAll('#mudalnouv input[type="Text"]');
+    //     let inputurl = document.querySelector('#mudalnouv input[type="url"]');
+    //     let inputnumber = document.querySelectorAll('#mudalnouv input[type="number"]');
+
+    //     infjoueur.forEach(Element =>{
+    //         if (Element.value == "") {
+    //         }
+    //         else{
                 
-            }
-        });
-    }
+    //         }
+    //     });
+    // };
 });
