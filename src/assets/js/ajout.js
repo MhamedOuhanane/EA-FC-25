fa07a71c62fab4095f76a880ecb963ee9e201f52
MhamedOuhanane <mhmdeouhnan60@gmail.com>
2024-11-29 01:54:46 +0100

    Séparation le code d'ajout un nouveau joueur par le code d'ajout des joueur au térrain par la céation d'un nouvelle fichier script qui s'appelle Rejex.js

--- src/assets/js/ajout.js
+++ src/assets/js/ajout.js
@@ -1,57 +1,6 @@
 //importer les données json des annonces à partir de son chemin
 import joueurs from "../data/players.json" with{type: "json"}
 
-//Ajouter un nouveau joueur
-Ajounouv.addEventListener("click" , ()=>{
-    mudalnouv.classList.remove("hidden");
-    cancelnouv.onclick= ()=>{
-        mudalnouv.classList.add("hidden");
-    };
-    let inputNGK = document.querySelectorAll(".inputNGK");
-    inputNGK.forEach(Element=>{
-        Element.classList.add("hidden");
-    });
-
-    function POSITIONSELECT() {
-        console.log(document.querySelectorAll(".inputNGK"));
-        if (positionnouv.value == "GK") {
-            document.querySelectorAll(".inputGK").forEach(Element=>{
-                Element.classList.remove("hidden");
-            });
-            inputNGK.forEach(Element=>{
-                Element.classList.add("hidden");
-            });
-        } else {
-            inputNGK.forEach(Element=>{  
-                Element.classList.remove("hidden");
-            });
-            document.querySelectorAll(".inputGK").forEach(Element=>{  
-                Element.classList.add("hidden");
-            });
-            
-        };
-    };
-    positionnouv.addEventListener("change", ()=>{
-        POSITIONSELECT();
-    });
-
-    let nouvjoue = {};
-    Create.onclick= () =>{
-        validation = false;
-        let infjoueur = document.querySelectorAll("#mudalnouv input");
-        infjoueur.forEach(Element =>{
-            if (Element.value == "") {
-                Element.ariaPlaceholder = "Veuillez entrer une valeur!";
-                Element.style.border = "2px solid red";
-                validation = false;
-            }
-            else{
-                
-            }
-        });
-    }
-});
-
 
 //ajouter un joueur au terrain
 //A partir de form
@@ -159,6 +108,8 @@ conferjoue.onclick= () =>{
             
         });
     };
+
+    // L'ajout des joueurs à partir d'un form
 
     BadgeAjout1.forEach((Element , index) =>{
         Element.onclick= () =>{
