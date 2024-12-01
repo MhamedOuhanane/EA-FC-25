30af54f26c6f627cf4e83a9116eaf25385e66b73
MhamedOuhanane <mhmdeouhnan60@gmail.com>
2024-12-01 21:21:23 +0100

    souvgarder les information des joueurs de la formation dans un array

--- src/assets/js/localstorage.js
+++ src/assets/js/localstorage.js
@@ -2,7 +2,30 @@
 import joueurs from "../data/players.json" with{type: "json"}
 
 //save les joueurs qui appartient au terrain et au remplaçant
-let formation = [];
-function SAVE(){
+// let formation = [];
+let container = document.querySelectorAll(".badgebouton");
+
+souvgarder.onclick = () =>{
+    let formation = [];
+    container.forEach(Element => {
+        let parentbag = Element.parentNode;
+        if (parentbag.children.length == 2) {
+            let name = parentbag.children[1].children[2].textContent;
+            let positionjoue = "";
+            joueurs.players.forEach(element =>{
+                if (name == element.name) {
+                    positionjoue = element.position;
+                };
+            });
+            let Effectif = parentbag.parentNode.id;
+            let info = {
+                name: name,
+                position: positionjoue,
+                effectif: Effectif
+            };
+            formation.push(info);
+        };
+    });
+    console.log(formation);
     
-}
+};
